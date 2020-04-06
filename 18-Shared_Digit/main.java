b309f282134cd77a381ac429c3cdfba825cf1519
Piyush <ankurp99@gmail.com>
2020-04-07 02:29:43 +0530

    get sum of even digit with while loop

---
+++ 18-Shared_Digit/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("The sum of even digits are = " + EvenDigitSum.getEvenDigitSum(123456789));
+        System.out.println("The sum of even digits are = " + EvenDigitSum.getEvenDigitSum(-26));
+    }
+}
