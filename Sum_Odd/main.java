47678dff1870b19ec401fb41d4542974a804dd4d
Piyush <ankurp99@gmail.com>
2020-04-06 17:25:32 +0530

    For loop usage

---
+++ Sum_Odd/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+
+        System.out.println("The result is - " + SumOddRange.isOdd(5));
+        System.out.println("The sum is = " + SumOddRange.sumOdd(5, 10));
+    }
+}
