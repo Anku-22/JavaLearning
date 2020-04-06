d56f5e8b8eaaf184c2c1656b6f3dde89e5c71dca
Piyush <ankurp99@gmail.com>
2020-04-07 00:37:29 +0530

    delete dir

--- Sum_Odd/main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-
-        System.out.println("The result is - " + SumOddRange.isOdd(5));
-        System.out.println("The sum is = " + SumOddRange.sumOdd(5, 10));
-    }
-}
