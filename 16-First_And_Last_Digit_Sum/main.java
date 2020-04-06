4f1093ee9b04c03a3ae32c016c3397097b254466
Piyush <ankurp99@gmail.com>
2020-04-07 01:22:44 +0530

    using while loop

---
+++ 16-First_And_Last_Digit_Sum/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("The sum of first and lst digit is : " + FirstLastDigitSum.sumFirstAndLastDigit(252));
+        System.out.println("The sum of first and lst digit is : " + FirstLastDigitSum.sumFirstAndLastDigit(257));
+        System.out.println("The sum of first and lst digit is : " + FirstLastDigitSum.sumFirstAndLastDigit(0));
+        System.out.println("The sum of first and lst digit is : " + FirstLastDigitSum.sumFirstAndLastDigit(5));
+        System.out.println("The sum of first and lst digit is : " + FirstLastDigitSum.sumFirstAndLastDigit(-10));
+    }
+}
