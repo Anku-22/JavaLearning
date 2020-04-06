7c8d3e585a6c72db8ebaa25b4ef68fcc5d6eea0f
Piyush <ankurp99@gmail.com>
2020-04-06 10:10:27 +0530

    Solution using Method Overloading

---
+++ Area_Calculator/main.java
@@ -0,0 +1,12 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Area of circle = " + AreaCalculator.area(5.0));
+        System.out.println("Area of circle = " + AreaCalculator.area(-1));
+
+        System.out.println("Area of circle = " + AreaCalculator.area(5.0, 4.0));
+        System.out.println("Area of circle = " + AreaCalculator.area(-1.0, 4.0));
+
+    }
+}
